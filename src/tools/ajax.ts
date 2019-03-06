d63195d8b0f7fa9a0d0996ed2510ad0a9c0f1708
Blame Hofmann <imnone0000@gmail.com>
2019-03-06 19:25:05 -0300

    Cambios en la estructura del Módulo de Ajax.ts

--- src/tools/ajax.ts
+++ src/tools/ajax.ts
@@ -1,66 +1,55 @@
 import * as http from "http"
 
 module Ajax {
-  export let get = (url: string, callback: (msg: http.IncomingMessage, data: any) => void, error?: () => void) => {
-    http.get(url, msg => {
-      let arr_byte: string | Buffer = null
 
-      msg.on("data", chunk => {
-        if (arr_byte == null) {
-          arr_byte = chunk
-        } else {
-          arr_byte += chunk
-        }
-      })
+  let fnCallback: (msg: http.IncomingMessage, data: any) => void
 
-      msg.on("end", () => {
-        let data: any
-        if (Buffer.isBuffer(arr_byte) == true) {
-          data = arr_byte.toString("utf8")
-        } else {
-          data = arr_byte
-        }
+  //Función que arma los datos a ejecutar
+  let makeRequest = (msg: http.IncomingMessage) => {
+    let arr_byte: string | Buffer = null
 
-        try {
-          data = JSON.parse(data)
-        } catch (err) {
-        }
+    msg.on("data", chunk => {
+      if (arr_byte == null) {
+        arr_byte = chunk
+      } else {
+        arr_byte += chunk
+      }
+    })
 
-        callback(msg, data)
-      })
-    }).on("error", () => {
+    msg.on("end", () => {
+      let data: any
+      if (Buffer.isBuffer(arr_byte) == true) {
+        data = arr_byte.toString("utf8")
+      } else {
+        data = arr_byte
+      }
+
+      try {
+        data = JSON.parse(data)
+      } catch (err) {
+      }
+
+      fnCallback(msg, data)
+    })
+  }
+
+  export let get = (url: string, callback: (msg: http.IncomingMessage, data: any) => void, error?: (err?: Error) => void) => {
+    fnCallback = callback
+    http.get(url, makeRequest).on("error", (err) => {
       if (error != null) {
-        error()
+        error(err)
       }
     })
   }
 
-  export let post = (url: string, options: http.RequestOptions, callback: (msg: http.IncomingMessage, data: any) => void, error?: () => void) => {
-    http.get(url, options, msg => {
-      let arr_byte: Buffer
+  export let post = (url: string, callback: (msg: http.IncomingMessage, data: any) => void, error?: (err?: Error) => void) => {
+    let options: http.RequestOptions = {}
+    options.method = "POST"
 
-      msg.on("data", chunk => {
-        arr_byte += chunk
-      })
-
-      msg.on("end", () => {
-        let data: any
-        if (Buffer.isBuffer(arr_byte) == true) {
-          data = arr_byte.toString("utf8")
-        } else {
-          data = arr_byte
-        }
-
-        try {
-          data = JSON.parse(data)
-        } catch (err) {
-        }
-
-        callback(msg, data)
-      })
-    }).on("error", () => {
+    fnCallback = callback
+    http.get(url, options, makeRequest).on("error", (err) => {
       if (error != null) {
-        error()
+        error(err)
       }
     })
   }
