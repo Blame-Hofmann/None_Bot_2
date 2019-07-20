a107f85f2a3d8bf8175b13856b58119950e0f87b
Blame-Hofmann <imnone0000@gmail.com>
2019-07-20 17:30:14 -0400

    Add Purge Command

--- src/driver/arr_cmd.ts
+++ src/driver/arr_cmd.ts
@@ -3,6 +3,7 @@ import Command from ">/tools/command"
 //Put commands Here!
 import cmd_debug from ">/response/cmd/debug/cmd_debug"
 import cmd_channel from ">/response/cmd/channel/cmd_channel"
+import cmd_channel_purge from ">/response/cmd/channel/cmd_channel_purge"
 import cmd_help from ">/response/cmd/misc/cmd_help"
 import cmd_currency from ">/response/cmd/misc/cmd_currency"
 import cmd_flip from ">/response/cmd/misc/cmd_flip"
@@ -13,6 +14,7 @@ import cmd_avatar from ">/response/cmd/misc/cmd_avatar"
 let arr_cmd: Array<Command> = [
   cmd_debug,
   cmd_channel,
+  cmd_channel_purge,
   cmd_help,
   cmd_currency,
   cmd_flip,
